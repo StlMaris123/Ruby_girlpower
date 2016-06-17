27f2512214d1da2f6ee0367db2dda1cbd428d174
cesswairimu <cessmbuguar@gmail.com>
2016-06-17 17:48:42 +0300

    removed repetition in staticpages tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,29 +1,36 @@
 require 'test_helper'
 
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+def setup
+ @common_title = "| Cess and Stella Awesome App"
+end
+
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home |Cess and Stella Awesome App"
+    assert_select "title", "Home #{@common_title}"
+    # assert_select "title", "Home | Cess and Stella Awesome App"
 
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About |Cess and Stella Awesome App"
+    assert_select "title", "About #{@common_title}"
   end
 
   test "should get faq" do
     get faq_url
     assert_response :success
-    assert_select "title", "FAQ |Cess and Stella Awesome App"
+    assert_select "title", "FAQ #{@common_title}"
   end
 
   test "should get contact" do
     get contact_url
     assert_response :success
-    assert_select "title", "Contact |Cess and Stella Awesome App"
+    assert_select "title", "Contact #{@common_title}"
   end
 
 end
