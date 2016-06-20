99fefcd2fe281aed30c58567e9b85adc70d5704b
cesswairimu <cessmbuguar@gmail.com>
2016-06-20 17:03:16 +0300

    validate email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+   VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, length: { maximum: 20 },
+                    uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 },
+          format: {   with: VALID_REGEX },
+          uniqueness: true
 end
